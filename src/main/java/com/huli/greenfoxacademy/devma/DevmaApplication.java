51a1fc70264ed9c5f8a1a1e2c681d7e45bcbe408
Levente Belteky <belteky.levente@gmail.com>
2018-08-06 08:39:00 +0200

    feat(app): init Spring Boot application

---
+++ src/main/java/com/huli/greenfoxacademy/devma/DevmaApplication.java
@@ -0,0 +1,12 @@
+package com.huli.greenfoxacademy.devma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DevmaApplication.class, args);
+  }
+}
---
+++ src/test/java/com/huli/greenfoxacademy/devma/DevmaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.huli.greenfoxacademy.devma;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevmaApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
