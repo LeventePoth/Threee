c1a59d0e3e311f59570b8831685cb3b645f4f32f
Niki <nikidobos@gmail.com>
2018-08-07 10:17:56 +0200

    refact(devma) change getter-setter

--- src/main/java/com/huli/greenfoxacademy/devma/Partners/Partner.java
+++ src/main/java/com/huli/greenfoxacademy/devma/Partners/Partner.java
@@ -1,11 +1,16 @@
 package com.huli.greenfoxacademy.devma.Partners;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Partner {
 
   @Id
@@ -27,62 +32,6 @@ public class Partner {
     this.companyName = companyName;
     this.email = email;
     this.password = password;
-    this.activated = activated;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-
-  public String getCompanyName() {
-    return companyName;
-  }
-
-  public void setCompanyName(String companyName) {
-    this.companyName = companyName;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public boolean isActivated() {
-    return activated;
-  }
-
-  public void setActivated(boolean activated) {
     this.activated = activated;
   }
 }
